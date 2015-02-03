470f25e09fe151bfb18c9e6693f3ec4c56ec1ead
Carl Yeksigian <carl@apache.org>
2015-02-03 14:16:30 -0500

    CFS, mutations in GI

--- src/java/org/apache/cassandra/config/GlobalIndexDefinition.java
+++ src/java/org/apache/cassandra/config/GlobalIndexDefinition.java
@@ -1,8 +1,11 @@
 package org.apache.cassandra.config;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import org.apache.cassandra.cql3.ColumnIdentifier;
+import org.apache.cassandra.db.Keyspace;
+import org.apache.cassandra.db.index.GlobalIndex;
 
 public class GlobalIndexDefinition
 {
@@ -15,5 +18,14 @@ public class GlobalIndexDefinition
         this.indexName = indexName;
         this.target = target;
         this.denormalized = denormalized;
+    }
+
+    public GlobalIndex resolve(CFMetaData cfm)
+    {
+        ColumnDefinition targetCd = cfm.getColumnDefinition(target);
+        Collection<ColumnDefinition> denormalizedCds = new ArrayList<>();
+        for (ColumnIdentifier cd: denormalized)
+            denormalizedCds.add(cfm.getColumnDefinition(cd));
+        return new GlobalIndex(indexName, targetCd, denormalizedCds, Keyspace.open(cfm.ksName).getColumnFamilyStore(cfm.cfName));
     }
 }
