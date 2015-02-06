c0e81e65c775e95fe43293aa6b00687d5a0acb09
Carl Yeksigian <carl@yeksigian.com>
2015-02-06 05:41:00 -0500

    Starting to allow selection from the selector itself

--- src/java/org/apache/cassandra/db/index/global/GlobalIndexSelectorOnPartitionKey.java
+++ src/java/org/apache/cassandra/db/index/global/GlobalIndexSelectorOnPartitionKey.java
@@ -1,12 +1,22 @@
 package org.apache.cassandra.db.index.global;
 
 import org.apache.cassandra.config.ColumnDefinition;
+import org.apache.cassandra.db.ColumnFamily;
+import org.apache.cassandra.db.ColumnFamilyStore;
 import org.apache.cassandra.db.composites.CellName;
+import org.apache.cassandra.db.marshal.CompositeType;
+
+import java.nio.ByteBuffer;
 
 public class GlobalIndexSelectorOnPartitionKey extends GlobalIndexSelector
 {
-    public GlobalIndexSelectorOnPartitionKey(ColumnDefinition cfDef)
+    ColumnFamilyStore baseCfs;
+    ColumnDefinition columnDef;
+
+    public GlobalIndexSelectorOnPartitionKey(ColumnFamilyStore baseCfs, ColumnDefinition cfDef)
     {
+        this.baseCfs = baseCfs;
+        this.columnDef = cfDef;
     }
 
     public boolean canGenerateTombstones()
@@ -16,7 +26,15 @@ public class GlobalIndexSelectorOnPartitionKey extends GlobalIndexSelector
 
     public boolean selects(CellName cellName)
     {
-        return false;
+        return true;
     }
 
+    @Override
+    public ByteBuffer value(CellName cellName, ByteBuffer key, ColumnFamily cf) {
+        if (columnDef.isOnAllComponents())
+            return key;
+        CompositeType keyComparator = (CompositeType)baseCfs.metadata.getKeyValidator();
+        ByteBuffer[] components = keyComparator.split(key);
+        return components[columnDef.position()];
+    }
 }
--- src/java/org/apache/cassandra/db/index/global/GlobalIndexSelectorOnRegularColumn.java
+++ src/java/org/apache/cassandra/db/index/global/GlobalIndexSelectorOnRegularColumn.java
@@ -1,9 +1,12 @@
 package org.apache.cassandra.db.index.global;
 
 import org.apache.cassandra.config.ColumnDefinition;
+import org.apache.cassandra.db.ColumnFamily;
 import org.apache.cassandra.db.ColumnFamilyStore;
 import org.apache.cassandra.db.composites.CellName;
 import org.apache.cassandra.db.marshal.AbstractType;
+
+import java.nio.ByteBuffer;
 
 public class GlobalIndexSelectorOnRegularColumn extends GlobalIndexSelector
 {
@@ -26,6 +29,10 @@ public class GlobalIndexSelectorOnRegularColumn extends GlobalIndexSelector
         AbstractType<?> comp = baseCfs.metadata.getColumnDefinitionComparator(columnDef);
         return name.size() > columnDef.position()
                 && comp.compare(name.get(columnDef.position()), columnDef.name.bytes) == 0;
+    }
+
+    public ByteBuffer value(CellName cellName, ByteBuffer key, ColumnFamily cf) {
+        return cf.getColumn(cellName).value();
     }
 
 }
