2c18dc329843c184612317385fe2c24fdf69182d
Carl Yeksigian <carl@apache.org>
2015-03-11 12:36:37 -0400

    Keep track of ending generation number

--- src/java/org/apache/cassandra/db/index/global/GlobalIndexBuilder.java
+++ src/java/org/apache/cassandra/db/index/global/GlobalIndexBuilder.java
@@ -18,8 +18,13 @@
 package org.apache.cassandra.db.index.global;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 
 import org.apache.cassandra.config.GlobalIndexDefinition;
 import org.apache.cassandra.db.*;
@@ -27,25 +32,25 @@ import org.apache.cassandra.db.index.GlobalIndex;
 import org.apache.cassandra.dht.Range;
 import org.apache.cassandra.dht.Token;
 import org.apache.cassandra.io.sstable.ReducingKeyIterator;
+import org.apache.cassandra.io.sstable.format.SSTableReader;
 import org.apache.cassandra.service.StorageProxy;
 import org.apache.cassandra.service.StorageService;
 import org.apache.cassandra.service.pager.QueryPagers;
+import org.apache.cassandra.utils.Pair;
 
 public class GlobalIndexBuilder
 {
     private final ColumnFamilyStore baseCfs;
     private final GlobalIndexDefinition definition;
     private final GlobalIndex index;
-    private final ReducingKeyIterator iter;
 
     private volatile boolean isStopped = false;
 
-    public GlobalIndexBuilder(ColumnFamilyStore baseCfs, GlobalIndexDefinition definition, ReducingKeyIterator iter)
+    public GlobalIndexBuilder(ColumnFamilyStore baseCfs, GlobalIndexDefinition definition)
     {
         this.baseCfs = baseCfs;
         this.definition = definition;
         this.index = definition.resolve(baseCfs.metadata);
-        this.iter = iter;
     }
 
     private void indexKey(DecoratedKey key)
@@ -67,7 +72,35 @@ public class GlobalIndexBuilder
             return;
 
         Iterable<Range<Token>> ranges = StorageService.instance.getLocalRanges(baseCfs.metadata.ksName);
-        ByteBuffer lastKey = SystemKeyspace.getGlobalIndexBuildStatus(ksname, indexname);
+        final Pair<Integer, ByteBuffer> indexStatus = SystemKeyspace.getGlobalIndexBuildStatus(ksname, indexname);
+        ReducingKeyIterator iter;
+        ByteBuffer lastKey;
+        // Need to figure out where to start
+        if (indexStatus == null)
+        {
+            int generation = Integer.MIN_VALUE;
+            Collection<SSTableReader> sstables = baseCfs.getSSTables();
+            for (SSTableReader reader: sstables)
+            {
+                generation = Math.max(reader.descriptor.generation, generation);
+            }
+            SystemKeyspace.beginGlobalIndexBuild(ksname, indexname, generation);
+            iter = new ReducingKeyIterator(sstables);
+            lastKey = null;
+        }
+        else
+        {
+            Collection<SSTableReader> sstables = Lists.newArrayList(Iterables.filter(baseCfs.getSSTables(), new Predicate<SSTableReader>()
+            {
+                @Override
+                public boolean apply(SSTableReader ssTableReader)
+                {
+                    return ssTableReader.descriptor.generation < indexStatus.left;
+                }
+            }));
+            iter = new ReducingKeyIterator(sstables);
+            lastKey = indexStatus.right;
+        }
 
         while (!isStopped && iter.hasNext())
         {
