5987fd52171d28e62ed43224658d13bd14cbe8ae
T Jake Luciani <jake@apache.org>
2015-05-14 13:58:24 -0400

    avoid string concat

--- src/java/org/apache/cassandra/cql3/statements/BatchStatement.java
+++ src/java/org/apache/cassandra/cql3/statements/BatchStatement.java
@@ -243,7 +243,7 @@ public class BatchStatement implements CQLStatement
         {
             Set<String> ksCfPairs = new HashSet<>();
             for (ColumnFamily cf : cfs)
-                ksCfPairs.add(cf.metadata().ksName + "." + cf.metadata().cfName);
+                ksCfPairs.add(String.format("%s.%s",cf.metadata().ksName, cf.metadata().cfName));
 
             String format = "Batch of prepared statements for {} is of size {}, exceeding specified threshold of {} by {}.";
             logger.warn(format, ksCfPairs, size, warnThreshold, size - warnThreshold);
@@ -261,7 +261,7 @@ public class BatchStatement implements CQLStatement
             {
                 keySet.add(im.key());
                 for (ColumnFamily cf : im.getColumnFamilies())
-                    ksCfPairs.add(cf.metadata().ksName + "." + cf.metadata().cfName);
+                    ksCfPairs.add(String.format("%s.%s",cf.metadata().ksName, cf.metadata().cfName));
             }
 
             NoSpamLogger.log(logger, NoSpamLogger.Level.WARN, 1, TimeUnit.MINUTES, unloggedBatchWarning,
